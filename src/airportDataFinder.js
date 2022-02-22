63cbf2cdeb541ed401e43807e202f70c871cc5a9
Nandika <Nandika_Arora@mckinsey.com>
2022-02-22 21:29:47 +0530

    feat: add airport information finder

---
+++ src/airportDataFinder.js
@@ -0,0 +1,25 @@
+const airportData = require('../data/airports.json');
+
+const getInformation = (airportCode) => {
+  if (typeof airportCode !== 'string') {
+    return 'invalid datatype';
+  }
+  if (airportCode.length !== 3) {
+    return 'invalid code';
+  }
+  const requiredAirportData = {};
+  airportData.forEach((airport) => {
+    if (airport.code === airportCode) {
+      requiredAirportData.name = airport.name;
+      requiredAirportData.city = airport.city;
+      requiredAirportData.country = airport.country;
+    }
+  });
+  if (Object.keys(requiredAirportData).length === 0) {
+    return 'Airport not found!';
+  }
+  return requiredAirportData;
+};
+
+// console.log(getInformation("a"));
+module.exports = getInformation;
